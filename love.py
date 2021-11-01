b2e2536cd92f9201af126d0eb7c60d4eda24202f
5nu <87293458+5nu@users.noreply.github.com>
2021-11-01 07:34:42 +0300

    Add files via upload

---
+++ love.py
@@ -0,0 +1,27 @@
+import turtle as t
+pen1 = t.Turtle()
+pen2 = t.Turtle()
+pen1.color("Cadmium green")
+pen1.width(3)
+pen2.color("red")
+pen2.width(3)
+
+pen2_name = "love"
+
+if pen2_name == "love":
+    pen2.left(40)
+    pen1.left(144)
+
+    pen2.forward(100)
+    pen1.forward(100)
+
+    for side in range(185):
+        pen2.forward(1)
+        pen1.forward(1)
+        pen2.left(1)
+        pen1.right(1)
+
+    pen2.hideturtle()
+    pen1.hideturtle()
+
+t.done()
